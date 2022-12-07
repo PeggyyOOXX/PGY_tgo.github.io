22ed095547d28153d5e0f335017c1231ee4bc540
PeggyyOOXX <115986530+PeggyyOOXX@users.noreply.github.com>
2022-12-07 16:53:07 +0800

    1207.v1

---
+++ assets/js/dropdown-menu.js
@@ -0,0 +1,4 @@
+const SELECTOR_MENU = '.dropdown-menu';
+const SELECTOR_VISIBLE_ITEMS = '.dropdown-menu .dropdown-item:not(.disabled):not(:disabled)';
+const CLASS_NAME_DROPDOWN_MENU = 'dropdown-menu';
+const SELECTOR_DROPDOWN_ACTIVE_CHILD = ':scope > .dropdown-menu .active';
